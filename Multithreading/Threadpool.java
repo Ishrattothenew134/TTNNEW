f06bcb5d400fff4735ea9bdc04c36af3ad135b21
ishirm98@gmail.com <ishirm98@gmail.com>
2020-02-20 22:26:39 +0530

    multithreading commit

---
+++ Multithreading/Threadpool.java
@@ -0,0 +1,49 @@
+//Q4.Write a program to create a Thread pool of 2 threads where one Thread will print even numbers and other will print odd numbers.
+package Multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Even implements Runnable{
+int num = 1;
+    public void run(){
+        while(true) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (num % 2 == 0 && num<=10) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
+
+class Odd implements Runnable{
+    int num = 1;
+    public void run(){
+        while(true) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (num % 2 != 0 && num<=10) {
+                System.out.println(num);
+            }
+            num++;
+        }
+    }
+}
+
+public class Threadpool {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(new Even());
+        executorService.submit(new Odd());
+
+        executorService.shutdown();
+    }
+}
