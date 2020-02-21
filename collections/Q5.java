4d50203d61ffe21b485e1edf2aca7f5508153cb3
ishirm98@gmail.com <ishirm98@gmail.com>
2020-02-21 17:11:40 +0530

    collection

---
+++ collections/Q5.java
@@ -0,0 +1,51 @@
+package collections;/* Write a program to sort Employee objects based on highest salary using Comparator.
+Employee class{ Double Age; Double Salary; String Name
+*/
+
+import java.util.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Employee {
+    private int age;
+    private double salary;
+    private String name;
+    public Employee(String name, Double salary, int age){
+        this.age = age;
+        this.salary = salary;
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return " {name: " + name + " age: " + age + " salary: " + salary + "}";
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+}
+class Q5{
+    public static void main(String[] args) {
+        List<Employee> list = new ArrayList<Employee>();
+        list.add(new Employee("Ricky", 434.45, 28));
+        list.add(new Employee("Pat", 942.25, 25));
+        list.add(new Employee("Pattrick", 734.00, 24));
+        list.add(new Employee("Ponting", 534.00, 26));
+        list.add(new Employee("Tendulkar", 124.45, 25));
+        list.add(new Employee("Mithali", 5534.00, 27));
+        Collections.sort(list, new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return -o1.getSalary().compareTo(o2.getSalary());
+            }
+        });
+        for(Employee values : list) {
+            System.out.println(values);
+        }
+    }
+}
---
+++ collections/Q6.java
@@ -0,0 +1,80 @@
+package collections;/* Write a program to sort the Student objects based on Score , if the score are same then sort on First Name .
+Class Student{ String Name; Double Score; Double Age
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+class Student implements Comparable<Student>{
+    private String name;
+    private Double score;
+    private int age;
+
+    public Student(String name, double score, int age){
+        this.name = name;
+        this.score = score;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Student obj) {
+        if(score > obj.score) {
+            return -1;
+        }
+        else if(score < obj.score) {
+            return 1;
+        }
+        else{
+            return name.compareTo(obj.name);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return " {name: " + name + " age: " + age + " score: " + score + "}";
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((score == null) ? 0 : score.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Student other = (Student) obj;
+        if (score == null) {
+            if (other.score != null)
+                return false;
+        } else if (!score.equals(other.score))
+            return false;
+        return true;
+    }
+    
+}
+
+public class Q6 {
+    public static void main(String[] args) {
+        List<Student> list = new ArrayList<Student>();
+        list.add(new Student("Champa", 94.24, 25));
+        list.add(new Student("Zampa", 75.34, 24));
+        list.add(new Student("Niti", 45.34, 28));
+        list.add(new Student("Harish", 85.34, 26));
+        list.add(new Student("Nav", 94.24, 25));
+        list.add(new Student("Richa", 55.34, 27));
+        Collections.sort(list);
+        for(Student values : list) {
+            System.out.println(values);
+        }
+        
+    }
+}
